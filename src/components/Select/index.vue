02bb4c7d225f79ffe605741f15d35228d20f1d62
weihong19971222 <46885087+weihong19971222@users.noreply.github.com>
2023-05-12 00:33:57 +0800

    feat:下拉元件

--- src/components/Select/index.vue
+++ src/components/Select/index.vue
@@ -1,24 +1,48 @@
 <script setup lang="ts">
-    import { ref } from "vue"
+    import { ref,computed } from "vue"
+
+    interface selectData {
+        id: number;
+        name: string;
+    }
+
     const props = defineProps({
-        data:{type: Array,default:() => ([])}
-    })
+        modelValue:{type: Array<selectData>},
+        width:{type: String, default:"200px"}
+    })    
+
+    const emit = defineEmits(['get-select-value'])
 
     const searchOpen = ref(false)
     const searchText = ref('')
-    const showOption = (e: Event) => {
-        e.target.firstChild.classList.toggle("open")
-        searchOpen.value = e.target.firstChild.classList.contains("open")
+    const datas = computed(() => {
+        if(searchText.value != ''){
+            let item: Array<selectData> = []
+            props.modelValue?.forEach(e=>{
+                if(e.name.includes(searchText.value)){
+                    item.push(e)
+                }
+            })
+            return item
+        }
+        return props.modelValue
+    })
+
+    const select = (item) => {
+        searchOpen.value = false
+        searchText.value = item.name
+        emit('get-select-value', item)
     };
+
 </script>
 <template>
-    <div class="srearch-select" @click="showOption">
-        <div class="srearch-select-option">
-            <!-- <div v-for="item in datas" :key="item.id" @click="select(item)">{{item.name}}</div> -->
+    <div :style="{width:props.width}" :class="['srearch-select',{'active':searchOpen}]" @click.self="searchOpen = !searchOpen">
+        <div class="srearch-select-show-search">
+            <input v-model="searchText" type="text" @focus="searchOpen = true">
+            <img src="@/assets/sideBarBtn.svg" @click="searchOpen = !searchOpen">
         </div>
-        <div class="srearch-select-show-search" @click.stop>
-            <!-- <div v-show="!searchOpen">{{showSelected}}</div> -->
-            <input v-show="searchOpen" v-model="searchText" type="text" >
+        <div v-show="searchOpen" class="srearch-select-option">
+            <div class="option" v-for="item in datas" :key="item.id" @click="select(item)">{{item.name}}</div>
         </div>
     </div>
 </template>
--- src/pages/headquarters/managedManagement/staff/index.vue
+++ src/pages/headquarters/managedManagement/staff/index.vue
@@ -1,10 +1,29 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref,reactive } from 'vue'
 import indexLayout from "@/layout/headquartersIndex.vue"
 import baseButton from "@/components/Button/index.vue"
 import searchSelect from "@/components/Select/index.vue"
 
 const searchValue = ref("")
+
+const obj = reactive([
+    {
+        id: 1,
+        name: "张三"
+    },
+    {
+        id: 2,
+        name: "李四"
+    },
+    {
+        id: 3,
+        name: "王五"
+    }
+])
+
+const getSelectValue = (item) => {
+    console.log(item.id);
+};
 
 </script>
 
@@ -13,7 +32,9 @@ const searchValue = ref("")
         v-model="searchValue">
         <div>
             <div :class="$style['select-group']">
-                <searchSelect></searchSelect>
+                <searchSelect
+                    v-model=obj
+                    @get-select-value="getSelectValue"></searchSelect>
             </div>
             <div :class="$style['store-staff-btn']">
                 <baseButton>新增員工</baseButton>
