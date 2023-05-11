72e7099393fd6bb2d83c34a924977a6b04f14800
weihong19971222 <46885087+weihong19971222@users.noreply.github.com>
2023-05-11 17:59:16 +0800

    feat:下拉篩選

---
+++ src/components/Select/index.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+    import { ref } from "vue"
+    const props = defineProps({
+        data:{type: Array,default:() => ([])}
+    })
+
+    const searchOpen = ref(false)
+    const searchText = ref('')
+    const showOption = (e: Event) => {
+        e.target.firstChild.classList.toggle("open")
+        searchOpen.value = e.target.firstChild.classList.contains("open")
+    };
+</script>
+<template>
+    <div class="srearch-select" @click="showOption">
+        <div class="srearch-select-option">
+            <!-- <div v-for="item in datas" :key="item.id" @click="select(item)">{{item.name}}</div> -->
+        </div>
+        <div class="srearch-select-show-search" @click.stop>
+            <!-- <div v-show="!searchOpen">{{showSelected}}</div> -->
+            <input v-show="searchOpen" v-model="searchText" type="text" >
+        </div>
+    </div>
+</template>
+<style lang="scss" scoped>
+    @import '@/assets/scss/select.scss';
+</style>
--- src/pages/headquarters/managedManagement/staff/index.vue
+++ src/pages/headquarters/managedManagement/staff/index.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 import indexLayout from "@/layout/headquartersIndex.vue"
+import baseButton from "@/components/Button/index.vue"
+import searchSelect from "@/components/Select/index.vue"
 
 const searchValue = ref("")
 
@@ -10,11 +12,23 @@ const searchValue = ref("")
     <indexLayout
         v-model="searchValue">
         <div>
-            
+            <div :class="$style['select-group']">
+                <searchSelect></searchSelect>
+            </div>
+            <div :class="$style['store-staff-btn']">
+                <baseButton>新增員工</baseButton>
+            </div>
         </div>
     </indexLayout>
 </template>
 
 <style lang="scss" module>
-
+.select-group{
+    margin-top: 20px;
+    display: flex;
+}
+.store-staff-btn{
+    margin-top: 15px;
+    text-align: right;
+}
 </style>
