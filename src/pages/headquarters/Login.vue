962a61da645c012c0852520de02c91882eb41e12
weihong19971222 <46885087+weihong19971222@users.noreply.github.com>
2023-05-15 09:46:04 +0800

    feat:登入頁縮放

--- src/pages/headquarters/Login.vue
+++ src/pages/headquarters/Login.vue
@@ -54,34 +54,36 @@ const fetchData = async (): Promise<any> => {
 </script>
 
 <template>
-    <div :class=$style.background>
-        <div :class=$style.mask>
-            <div :class=$style.title>村露國際</div>
-            <div>
-                <baseInput
-                    v-model="account_number"
-                    marginBottom="30px"
-                    inputWidth="279px"
-                    placeholderText="請輸入帳號"
-                    labelText="帳號"
-                    relativeDistance="35px"
-                    className="mb-30"
-                />
-                <baseInput
-                    v-model="password"
-                    marginBottom="56px"
-                    inputWidth="279px"
-                    placeholderText="請輸入密碼"
-                    labelText="密碼"
-                    relativeDistance="35px"
-                    inputType="password"
-                />
-                <baseButton
-                    style="width: 100%;"
-                    :onClick="()=>fetchData()"
-                >登入</baseButton>
-                <div :class="$style['to-store-href']" >前往<a href="/headquarters">門市後台</a></div>
-                <div :class="$style['hint-msg']"> <img src="@/assets/hint-icon.svg" /> 由總部人員註冊帳號</div>
+    <div style="height: 100vh;">
+        <div :class=$style.background>
+            <div :class=$style.mask>
+                <div :class=$style.title>村露國際</div>
+                <div>
+                    <baseInput
+                        v-model="account_number"
+                        marginBottom="30px"
+                        inputWidth="279px"
+                        placeholderText="請輸入帳號"
+                        labelText="帳號"
+                        relativeDistance="35px"
+                        className="mb-30"
+                    />
+                    <baseInput
+                        v-model="password"
+                        marginBottom="56px"
+                        inputWidth="279px"
+                        placeholderText="請輸入密碼"
+                        labelText="密碼"
+                        relativeDistance="35px"
+                        inputType="password"
+                    />
+                    <baseButton
+                        style="width: 100%;"
+                        :onClick="()=>fetchData()"
+                    >登入</baseButton>
+                    <div :class="$style['to-store-href']" >前往<a href="/headquarters">門市後台</a></div>
+                    <div :class="$style['hint-msg']"> <img src="@/assets/hint-icon.svg" /> 由總部人員註冊帳號</div>
+                </div>
             </div>
         </div>
     </div>
@@ -104,9 +106,10 @@ const fetchData = async (): Promise<any> => {
     flex-direction:column;
 }
 .background{
-    min-height: 1080px;
-    min-width: 1920px;
-    height: 100vh;
+    // min-height: 1080px;
+    // min-width: 1920px;
+    min-height: 100%;
+    // height: 100vh;
     display: flex;
     justify-content: center;
     align-items: center;
