6ecb3079bb7b55475c1591e01e60e82024e1d488
weihong19971222 <46885087+weihong19971222@users.noreply.github.com>
2023-05-23 17:44:51 +0800

    test

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory, RouteRecordRaw} from 'vue-router'
+import { createRouter, createWebHistory, RouteRecordRaw} from 'vue-router'
 import { getCookie } from 'typescript-cookie'
 
 // 創建一個 routes 陣列，定義所有 route 路徑
@@ -23,7 +23,7 @@ const routes: Array<RouteRecordRaw> = [
 
 // 創建一個router，
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes // 導入使用上方 routes 所定義的路徑
 })
 
